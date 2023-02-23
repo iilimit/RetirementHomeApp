05045141b1eb11451a352b5fa61c8271203346e8
Jordan <jordanmccord23@yahoo.com>
2023-02-23 00:00:42 -0500

    Added elements to home page and login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-//first push
 import 'package:flutter/material.dart';
+
+import 'home/home_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,68 +27,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Flutter FlatButton Example'),
-          ),
-          body: Center(
-              child: Column(children: <Widget>[
-            Container(
-              child: Text("Username"),
-            ),
-            Container(
-              child: Text("Password"),
-            ),
-            Container(
-              child: Text("OR"),
-            ),
-            Container(
-              margin: EdgeInsets.all(25),
-              child: ElevatedButton(
-                child: Text(
-                  'SignUp',
-                  style: TextStyle(fontSize: 20.0),
-                ),
-                onPressed: () {},
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.all(25),
-              child: ElevatedButton(
-                child: Text(
-                  'LogIn',
-                  style: TextStyle(fontSize: 20.0),
-                ),
-                onPressed: () {},
-              ),
-            ),
-          ]))),
     );
   }
 }
