d118df77534ee2fd729166a91cfcee67b4230ae0
Jordan <jordanmccord23@yahoo.com>
2023-02-26 00:32:41 -0500

    added nav bar widget to its own file

---
+++ lib/navigation/navigationbar_widget.dart
@@ -0,0 +1,206 @@
+import 'package:flutter/material.dart';
+
+import '../home/home_widget.dart';
+import '../login/login_widget.dart';
+
+class NavigationMenu extends StatelessWidget implements PreferredSizeWidget {
+  const NavigationMenu({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      // Add a ListView to the drawer. This ensures the user can scroll
+      // through the options in the drawer if there isn't enough vertical
+      // space to fit everything.
+      child: ListView(
+        // Important: Remove any padding from the ListView.
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text('Menu'),
+          ),
+          ListTile(
+            title: const Text('Home'),
+            onTap: () {
+              // Update the state of the app
+              // ...
+              // Then close the drawer
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const MyHomePage(
+                          title: 'Home Page',
+                        )),
+              );
+            },
+          ),
+          ListTile(
+            title: const Text('Request Nurse'),
+            onTap: () {
+              // Update the state of the app
+              // ...
+              // Then close the drawer
+              requestNurseAlertDialog(context);
+            },
+          ),
+          ListTile(
+            title: const Text('Meal Plan'),
+            onTap: () {
+              // Update the state of the app
+              // ...
+              // Then close the drawer
+            },
+          ),
+          ListTile(
+            title: const Text('Messages'),
+            onTap: () {
+              // Update the state of the app
+              // ...
+              // Then close the drawer
+            },
+          ),
+          ListTile(
+            title: const Text('Schedule'),
+            onTap: () {
+              // Update the state of the app
+              // ...
+              // Then close the drawer
+            },
+          ),
+          ListTile(
+            title: const Text('Settings'),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text('Logout'),
+            onTap: () {
+              // Update the state of the app
+              // ...
+              // Then close the drawer
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const LoginPage(
+                          title: 'Logout',
+                        )),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(60.0);
+}
+
+requestNurseAlertDialog(BuildContext context) {
+  // set up the buttons
+  Widget cancelButton = TextButton(
+    child: const Text("Cancel"),
+    onPressed: () {
+      Navigator.of(context).pop();
+    },
+  );
+  Widget continueButton = TextButton(
+    child: const Text("Yes"),
+    onPressed: () {
+      Navigator.of(context).pop();
+      requestNurseEmergencyAlertDialog(context);
+    },
+  );
+
+  // set up the AlertDialog
+  AlertDialog alert = AlertDialog(
+    title: const Text("Request nurse"),
+    content: const Text("Would you like to request a nurse?"),
+    actions: [
+      cancelButton,
+      continueButton,
+    ],
+  );
+
+  // show the dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
+}
+
+requestNurseEmergencyAlertDialog(BuildContext context) {
+  // set up the buttons
+  Widget cancelButton = TextButton(
+    child: const Text("Cancel"),
+    onPressed: () {
+      Navigator.of(context).pop();
+    },
+  );
+  Widget noButton = TextButton(
+    child: const Text("No"),
+    onPressed: () {
+      Navigator.of(context).pop();
+      requestNurseConfirmationAlertDialog(context);
+    },
+  );
+  Widget yesButton = TextButton(
+    child: const Text("Yes"),
+    onPressed: () {
+      Navigator.of(context).pop();
+      requestNurseConfirmationAlertDialog(context);
+    },
+  );
+
+  // set up the AlertDialog
+  AlertDialog alert = AlertDialog(
+    title: const Text("Confirm request..."),
+    content: const Text("Is this an emergency?"),
+    actions: [
+      cancelButton,
+      noButton,
+      yesButton,
+    ],
+  );
+
+  // show the dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
+}
+
+requestNurseConfirmationAlertDialog(BuildContext context) {
+  // set up the button
+  Widget okButton = TextButton(
+    child: Text("OK"),
+    onPressed: () {
+      Navigator.pop(context);
+    },
+  );
+
+  // set up the AlertDialog
+  AlertDialog alert = AlertDialog(
+    title: Text("Confirmed"),
+    content: Text("A nurse will be with you shortly!"),
+    actions: [
+      okButton,
+    ],
+  );
+
+  // show the dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
+}
