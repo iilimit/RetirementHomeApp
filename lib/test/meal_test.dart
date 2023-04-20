85526eec87f769e7ccfb055b4b7238c900d78dec
Jordan McCord <jordanmccord23@yahoo.com>
2023-04-20 14:15:35 -0400

    Created successful Meal test

--- lib/test/meal_test.dart
+++ lib/test/meal_test.dart
@@ -1,8 +1,14 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:project_1/mealplan/meal.dart';
+import 'package:project_1/meal_plan/meal.dart';
 
 void main() {
-  test("", () {
-    // Meal meal = Meal.fromJson(json)
-  });
+  test(
+    "Test valid Meal", 
+    () {
+    Meal meal = Meal.fromJson({"id":1, "type":"Breakfast", "combo":"Scammbled eggs, toast, sausage"});
+    expect(meal.id, 1);
+    expect(meal.type, "Breakfast");
+    expect(meal.combo, "Scammbled eggs, toast, sausage");
+  }
+  );
 }
