c4ee8a3ef1cbb901e427eb15792d1bd5338ecdd1
jmccord8 <jmccord8@my.westga.edu>
2023-04-30 13:58:44 -0400

    Added google maps function and 1 market

--- lib/map/map_screen.dart
+++ lib/map/map_screen.dart
@@ -1,21 +1,55 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import '../navigation/navigationbar_widget.dart';
 
 class MapPage extends StatefulWidget {
-  const MapPage({super.key, required this.title});
-  final String title;
+  const MapPage({Key? key, required String title}) : super(key: key);
 
   @override
-  State<MapPage> createState() => _MapPageState();
+  State<MapPage> createState() => MapSampleState();
 }
 
-class _MapPageState extends State<MapPage> {
+class MapSampleState extends State<MapPage> {
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
+  static const CameraPosition _initalPosition = CameraPosition(
+    target: LatLng(33.752098774130076, -84.39182326033728),
+    zoom: 14.4746,
+  );
+
+  static final Marker _gaAquariumMarker = const Marker(
+      markerId: MarkerId('_gaAquariumMarker'),
+      infoWindow: InfoWindow(title: 'Georgia Aquarium'),
+      icon: BitmapDescriptor.defaultMarker,
+      position: LatLng(33.763501097617166, -84.39459827012271));
+  static const CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: NavigationAppBar(title: "Map"),
-      drawer: NavigationMenu(),
+    return Scaffold(
+      appBar: const NavigationAppBar(title: "Meal Plan"),
+      drawer: const NavigationMenu(),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        markers: {_gaAquariumMarker},
+        initialCameraPosition: _initalPosition,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
     );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
